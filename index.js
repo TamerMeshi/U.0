efdb2a270dbcbffc4da91df6a776547d25ded4ec
TamerMeshi <td.meshi@gmail.com>
2023-08-03 20:37:06 +0300

    add h1 title

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+document.querySelector('.btn').onclick =  ()=>{
+    let i = 5;
+    let a = document.querySelector('.int').value;
+    if(a==i){
+    alert('         УГАДАЛ!!!!!');
+    }else{
+        
+        alert('      ПРОБУЙ ЕЩЕ!!!!')
+    }
+} 
